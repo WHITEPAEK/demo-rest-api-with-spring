71d054df1388f79c5d2b77c994412130e80d738d
WHITEPAEK <atomsjp@gmail.com>
2020-09-13 21:52:21 +0900

    Spring REST Docs 적용
    + 테스트 코드에 Rest Docs 자동 설정 (@AutoConfigureRestDocs)
    + RestDocsConfiguration 커스터마이징 적용
    + Request, Response : Field, Header 문서화 적용
    + Docs 문서 빌드 : 테스트 코드 실행 후 Maven-package
      (target -> generated-snippets, generated-docs 확인)

--- src/main/java/com/tistory/whitepaek/restapiwithspring/events/EventController.java
+++ src/main/java/com/tistory/whitepaek/restapiwithspring/events/EventController.java
@@ -2,6 +2,7 @@ package com.tistory.whitepaek.restapiwithspring.events;
 
 import org.modelmapper.ModelMapper;
 import org.springframework.hateoas.EntityModel;
+import org.springframework.hateoas.Link;
 import org.springframework.hateoas.MediaTypes;
 import org.springframework.hateoas.server.mvc.WebMvcLinkBuilder;
 import org.springframework.http.ResponseEntity;
@@ -53,6 +54,7 @@ public class EventController {
         EntityModel<Event> eventResource = EntityModel.of(event, selfLinkBuilder.withSelfRel());
         eventResource.add(linkTo(EventController.class).withRel("query-events"));
         eventResource.add(selfLinkBuilder.withRel("update-event"));
+        eventResource.add(Link.of("/docs/index.html#resources-events-create").withRel("profile"));
 
         return ResponseEntity.created(createdUri).body(eventResource);
     }
